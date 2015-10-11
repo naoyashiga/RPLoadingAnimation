02e57ac5f6ef71287c13dd8242d778fc987cfcb9
naoyashiga <naoyashiga0@gmail.com>
2015-10-11 17:37:16 +0900

    make RotatingCircle

---
+++ RPLoadingAnimation/RotatingCircle.swift
@@ -0,0 +1,63 @@
+//
+//  RotatingCircle.swift
+//  RPLoadingAnimation
+//
+//  Created by naoyashiga on 2015/10/11.
+//  Copyright © 2015年 naoyashiga. All rights reserved.
+//
+
+import UIKit
+
+class RotatingCircle {
+    
+    func setup(layer: CALayer, size: CGSize, color: UIColor) {
+        
+        let circle = CALayer()
+        let frame = CGRect(
+            x: (layer.bounds.width - size.width) / 2,
+            y: (layer.bounds.height - size.height) / 2,
+            width: size.width,
+            height: size.height
+        )
+        
+        circle.backgroundColor = color.CGColor
+        circle.cornerRadius = size.width / 2
+        circle.frame = frame
+        
+        let replicatorLayer = CAReplicatorLayer()
+        replicatorLayer.frame = layer.bounds
+        layer.addSublayer(replicatorLayer)
+        
+        replicatorLayer.addSublayer(circle)
+        replicatorLayer.instanceCount = 4
+        replicatorLayer.instanceTransform = CATransform3DMakeTranslation(20, 0, 0)
+        
+        let animation = CABasicAnimation(keyPath: "position.y")
+        animation.toValue = frame.origin.y + 50
+        animation.duration = 0.5
+        animation.autoreverses = true
+        animation.repeatCount = .infinity
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        circle.addAnimation(animation, forKey: "animation")
+        
+        let scaleAnimation = CABasicAnimation(keyPath: "transform.scale")
+        scaleAnimation.toValue = 0.8
+        scaleAnimation.duration = 0.5
+        scaleAnimation.autoreverses = true
+        scaleAnimation.repeatCount = .infinity
+        scaleAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        circle.addAnimation(scaleAnimation, forKey: "scaleAnimation")
+        
+        let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation")
+        rotationAnimation.toValue = -2.0 * M_PI
+        rotationAnimation.duration = 6.0
+        rotationAnimation.repeatCount = .infinity
+        rotationAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        replicatorLayer.addAnimation(rotationAnimation, forKey: "rotationAnimation")
+        
+        replicatorLayer.instanceDelay = 0.1
+        
+        let angle = (2.0 * M_PI) / Double(replicatorLayer.instanceCount)
+        replicatorLayer.instanceTransform = CATransform3DMakeRotation(CGFloat(angle), 0.0, 0.0, 1.0)
+    }
+}
--- RPLoadingAnimation/ViewController.swift
+++ RPLoadingAnimation/ViewController.swift
@@ -13,67 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let circle = UIView()
-        let circle = CALayer()
-        let radius: CGFloat = 30
-        
-        circle.backgroundColor = UIColor.blackColor().CGColor
-        circle.cornerRadius = radius / 2
-        circle.frame.origin = view.center.offset(dx: -radius / 2, dy: -radius / 2)
-        circle.frame.size = CGSize(width: radius, height: radius)
-        
-//        view.addSubview(circle)
-        
-        let replicatorLayer = CAReplicatorLayer()
-        replicatorLayer.frame = view.bounds
-        view.layer.addSublayer(replicatorLayer)
-        
-        replicatorLayer.addSublayer(circle)
-        replicatorLayer.instanceCount = 4
-        replicatorLayer.instanceTransform = CATransform3DMakeTranslation(20, 0, 0)
-        
-        
-        let animation = CABasicAnimation(keyPath: "position.y")
-        animation.toValue = view.center.y + 50
-        animation.duration = 0.5
-        animation.autoreverses = true
-        animation.repeatCount = .infinity
-        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        circle.addAnimation(animation, forKey: "animation")
-        
-        let scaleAnimation = CABasicAnimation(keyPath: "transform.scale")
-        scaleAnimation.toValue = 0.8
-        scaleAnimation.duration = 0.5
-        scaleAnimation.autoreverses = true
-        scaleAnimation.repeatCount = .infinity
-        scaleAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        circle.addAnimation(scaleAnimation, forKey: "scaleAnimation")
-        
-        let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation")
-        rotationAnimation.toValue = -2.0 * M_PI
-        rotationAnimation.duration = 6.0
-        rotationAnimation.repeatCount = .infinity
-        rotationAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        replicatorLayer.addAnimation(rotationAnimation, forKey: "rotationAnimation")
-        
-        replicatorLayer.instanceDelay = 0.1
-        
-        let angle = (2.0 * M_PI) / Double(replicatorLayer.instanceCount)
-        replicatorLayer.instanceTransform = CATransform3DMakeRotation(CGFloat(angle), 0.0, 0.0, 1.0)
-//        replicatorLayer.instanceAlphaOffset = 0.8
-//        UIView.animateWithDuration(
-//            2.0,
-//            delay: 0,
-//            options: [.Repeat, .Autoreverse],
-//            animations: {
-//                circle.center.y += 100
-//                
-//            }, completion: nil)
+        let animation = RotatingCircle()
+        let size = CGSize(width: 30, height: 30)
+        animation.setup(view.layer, size: size, color: UIColor.blackColor())
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
 
