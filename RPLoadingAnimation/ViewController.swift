25d2b535c7a07d85e29c0b1e09c21fefd9649c1e
naoyashiga <naoyashiga0@gmail.com>
2015-10-11 15:53:30 +0900

    Initial Commit

---
+++ RPLoadingAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RPLoadingAnimation
+//
+//  Created by naoyashiga on 2015/10/11.
+//  Copyright © 2015年 naoyashiga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
