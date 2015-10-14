83034f1bbab727b7a5bd55828a8e258a588431f8
naoyashiga <naoyashiga0@gmail.com>
2015-10-14 22:08:29 +0900

    add like sky loading animation

--- RPLoadingAnimation/DotSpinningLikeSkype.swift
+++ RPLoadingAnimation/DotSpinningLikeSkype.swift
@@ -14,9 +14,9 @@ class DotSpinningLikeSkype: RPLoadingAnimationDelegate {
     
     func setup(layer: CALayer, size: CGSize, color: UIColor) {
         
-        let dotNum: CGFloat = 10
-        let diameter: CGFloat = size.width / 10
-        let duration: CFTimeInterval = 2.5
+        let dotNum: CGFloat = 8
+        let diameter: CGFloat = size.width / 16
+        let duration: CFTimeInterval = 2.8
         
         let dot = CALayer()
         let frame = CGRect(
@@ -36,17 +36,16 @@ class DotSpinningLikeSkype: RPLoadingAnimationDelegate {
         let replicatorLayer = CAReplicatorLayer()
         replicatorLayer.frame = layer.bounds
         replicatorLayer.instanceCount = Int(dotNum)
-        replicatorLayer.instanceDelay = CFTimeInterval(-1 / dotNum)
+        replicatorLayer.instanceDelay = CFTimeInterval(-1 / dotNum * 0.5)
         
-        let angle = (2.0 * M_PI) / Double(replicatorLayer.instanceCount)
-        
+//        let angle = (2.0 * M_PI) / Double(replicatorLayer.instanceCount)
 //        replicatorLayer.instanceTransform = CATransform3DMakeRotation(CGFloat(angle), 0.0, 0.0, 1.0)
         
         layer.addSublayer(replicatorLayer)
         replicatorLayer.addSublayer(dot)
         
         let scaleAnimation = CABasicAnimation(keyPath: "transform.scale")
-        scaleAnimation.toValue = 0.3
+        scaleAnimation.toValue = 0.8
         scaleAnimation.duration = duration
         scaleAnimation.autoreverses = true
         scaleAnimation.repeatCount = .infinity
@@ -62,20 +61,21 @@ class DotSpinningLikeSkype: RPLoadingAnimationDelegate {
 //        dot.addAnimation(rotationAnimation, forKey: "rotationAnimation")
         
         let moveAnimation = CAKeyframeAnimation(keyPath: "position")
+        moveAnimation.beginTime = 1.0
         moveAnimation.path = getPath()
         moveAnimation.duration = duration
         moveAnimation.repeatCount = .infinity
-        moveAnimation.timingFunction = TimingFunction.EaseInOut.getTimingFunction()
+        moveAnimation.timingFunction = TimingFunction.EaseInOutCirc.getTimingFunction()
         dot.addAnimation(moveAnimation, forKey: "moveAnimation")
         
-//        layer.transform = CATransform3DMakeRotation(CGFloat(M_PI) / 2, 0, 0, 0)
+        layer.transform = CATransform3DMakeRotation(CGFloat(M_PI) / 2, 0, 0, 0)
     }
     
     func getPath() -> CGPath {
-        let diameter: CGFloat = baseSize.width * 2
+        let radius: CGFloat = baseSize.width / 3
+        let p = CGPathCreateMutable()
         
-        let rect = CGRectMake(-diameter / 4, -diameter / 4, diameter, diameter)
-        let p = CGPathCreateWithEllipseInRect(rect, nil)
+        CGPathAddArc(p, nil, (baseSize.width) / 2, (baseSize.height) / 2, radius, CGFloat(-M_PI_2), CGFloat(M_PI_2 * 3), false)
         return p
     }
 }
